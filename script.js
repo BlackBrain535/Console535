c60aea6d2e831428a9fa0d1b1256b56665d6c88f
BlackBrain535 <hronos.535@gmail.com>
2021-11-12 23:39:48 +0300

    0.3

--- script.js
+++ script.js
@@ -1,88 +1,171 @@
 
 
+let checkBot = false;
 
+/*
+Отдел: Получение обьектов 
+Отдел: Функции
+*/
+
+//Отдел: Получение обьектов
 const consoleText = document.getElementById('consoleText');
 const consoleInput = document.getElementById('consoleInput');
+//Отдел: Функции
+function showText(name='',colorName='white',text,colorText='white',colorBackground='rgba(0,0,0,0)'){
+    consoleText.innerHTML += `<div style=\'color:${colorText}; background:${colorBackground};\'><span style='color:${colorName}'>${name}:</span> ${text}</div>`
+}
 
 
-
-function console(text, color='none'){
-    if(color == 'none'){
-        consoleText.innerHTML += ''+text+'<br>';
-       
-    }
-    else if (color == 'red'){
-        consoleText.innerHTML += text+'<br><br>';
+function checkClickOk (){
+    if(consoleInput.value.indexOf('/') == 0){
         
+        
+        
+
+        if(consoleInput.value == '/BotFalse'){
+            showText('You','green',consoleInput.value,'white');
+            showText('Console', 'yellow', 'Bot:False', 'white');
+            checkBot = false;
+        }
+
+        else if(consoleInput.value == '/BotTrue'){
+            showText('You','green',consoleInput.value,'white');
+            showText('Console', 'yellow', 'Bot:True', 'white');
+            checkBot = true;
+        }
+
+
+
+        else if(consoleInput.value == '/help'){
+            doListCommand();
+        }
+        else if(consoleInput.value == '/clear'){
+            doCleare();
+        }
+        else if(consoleInput.value == '/listTask'){
+            doListTask();
+        }
+        else if(consoleInput.value.indexOf('/createTask') == 0){
+            let i = consoleInput.value.split(':');
+            let task = i[1];
+            
+            createTask(task);
+            showText('You','green',consoleInput.value,'white');
+            showText('Console', 'yellow', `create task: ${task}`, 'white');
+        }
+        else{
+            showText('Console','red','Error command "'+consoleInput.value+'" no exists', 'white');
+        }
     }
     
-}
-
-const obj = {};
-
-
-function ClickOk (){
-    
-    if(consoleInput.value == 'help'){
-        listCommand();
-    }
-    else if(consoleInput.value == 'clear'){
-        clear();
-    }
-    else if(consoleInput.value == 'listTask'){
-        listTask();
-    }
-    else if(consoleInput.value.indexOf('creatTask') != -1){
-        let i = consoleInput.value.split(':');
-        let task = i[1];
-        let value = i[2];
-        creatTask(task, value);
-        console('Creat task: Task='+task+',Value='+value);
-    }
     else{
-        console('Error: command "'+consoleInput.value+'" no exists', 'red');
-    }
-    consoleInput.value = '';
-}
+        showText('You','green',consoleInput.value,'white');
+        if(checkBot){
+            
+            listAnswer = [
+                [
+                    'Привет!',
+                    'Хай',
+                    'Хеллоу!',
+                    'Здравствуй!',
+                    'Здравствуйте!',
+                    'Приветствую!',
+                    'Рад видеть вас!',
+                    'Приветули!',
+                    'Приветики!',
+                    'Привет, Бро!',
+                    'Вот это люди!',
+                    'Здравствуйте! Царь!',
+                    'Даров!',
+                    'Дратути!',
+                    'Салам!',
+                    'Кукусики!',
+                    'Ку!',
+                    'Хаю Хай!',
+                ]
+                
+                
+            ];
 
-const Command = {
-    '1': 'help',
-    '2': 'clear',
-    '3': 'creatTask: task:value',
-    '4': 'listTask',
+ 
+            text = consoleInput.value;
+            const listHello = [
+                'прив',
+                'хай',
+                'здрав',
+                'ку',
+                'салам',
+                
+                
+            ];
+            
+            text = text.toLocaleLowerCase();
+            for (let elem of listHello){
+                
+                let i = Math.floor(Math.random()*listAnswer[0].length);
     
+                if(text.indexOf(elem) >= 0){
+                    showText('Bot', 'blue', listAnswer[0][i], 'white');
+                }
+            }
+
+
+
+
+            
+            
+
+
+         
+        }
+    }   
+    consoleInput.value = '';
 };
 
-function listCommand(){
-    console('>listTask')
-    for(let i in Command){
-        console(i+': '+Command[i]);
-    }
-   
-}
 
-
-function clear(){
-    
+const ListCommand = [
+    '/help',
+    '/clear',
+    '/createTask:task',
+    '/listTask',
+    '/BotTrue',
+    '/BotFalse',
+];
+function doListCommand(){
+    showText('You', 'green', consoleInput.value, 'white');
+    for(let i = 0; i<ListCommand.length; i++){   
+        showText(1+i,'yellow',ListCommand[i], 'white');
+    }  
+};
+function doCleare(){
     consoleText.innerHTML = '';
+};
+
+const ListTask = [];
+function createTask(task){
+    ListTask.push(task);
     
 }
+function doListTask(){
+    showText('You', 'green', consoleInput.value, 'white');
+    for(let i = 0; i<ListTask.length; i++){   
+        showText(1+i,'yellow',ListTask[i], 'white');
+        alert('asd');
+    }  
+};
 
 
 
-function listTask(){
-    let i = 1;
-    for(let task in objListTask){
-        
-        console('-'+i+')'+task+': '+objListTask[task]);
-        i++;
+function getConsoleVersion(){
+    return "0.1";
+}
+
+
+//Отдел Cобытиями
+document.addEventListener('keydown', function(event){
+    if(event.key == "Enter"){
+        checkClickOk();
+        console.log('Отдел: События (true)');
     }
-}
-
-
-const objListTask = {};
-
-function creatTask(task, value){
-    objListTask[task] = value;
-}
-
+    
+});
